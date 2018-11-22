fcef47c5df46686c59591de1724d0e7b03a7f005
simpart <simpart>
2018-11-22 04:38:52 +0000

    add comment

--- index.js
+++ index.js
@@ -6,7 +6,13 @@
 const mf = require('mofron');
 
 mf.effect.Margin = class extends mf.Effect {
-    
+    /**
+     * initialize effect
+     *
+     * @param p1 (object) effect option 
+     * @param p1 (string) margin type
+     * @param p2 (string) margin value (css size)
+     */
     constructor (po, p2) {
         try {
             super();
@@ -19,6 +25,11 @@ mf.effect.Margin = class extends mf.Effect {
         }
     }
     
+    /**
+     * enable margin effect
+     *
+     * @note private method
+     */
     enable (tgt) {
         try {
             let mg = (null === this.type()) ? 'margin' : 'margin' + '-' + this.type();
@@ -31,6 +42,11 @@ mf.effect.Margin = class extends mf.Effect {
         }
     }
     
+    /**
+     * disable margin effect
+     *
+     * @note private method
+     */
     disable (tgt) {
         try {
             let mg = (null === this.type()) ? 'margin' : 'margin' + '-' + this.type();
@@ -43,6 +59,15 @@ mf.effect.Margin = class extends mf.Effect {
         }
     }
     
+    /**
+     * margin value setter/getter
+     *
+     * @param p1 (string) enable margin value (size value of css)
+     * @param p1 (undefined) call as getter
+     * @param p2 (string) disable margin value (size value of css)
+     * @param p2 (undefined) call as getter
+     * @reutrn (array) [ enable value, disable value ]
+     */
     value (ena, dis) {
         try {
             if ( (undefined === ena) &&
@@ -59,6 +84,13 @@ mf.effect.Margin = class extends mf.Effect {
         }
     }
     
+    /**
+     * enable margin value setter/getter
+     *
+     * @param p1 (string) enable margin value (size value of css)
+     * @param p1 (undefined) call as getter
+     * @reutrn (string) enable value
+     */
     enaValue (prm) {
         try {
             let ret = this.member(
@@ -66,13 +98,20 @@ mf.effect.Margin = class extends mf.Effect {
                 ['Size'],
                 (undefined !== prm) ? mf.func.getSize(prm) : prm
             );
-            return (undefined !== ret) ? ret.toString() : undefined;
+            return (true === mf.func.isInclude(ret, 'Size')) ? ret.toString() : null;
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
+    /**
+     * disable margin value setter/getter
+     *
+     * @param p1 (string) disable margin value (size value of css)
+     * @param p1 (undefined) call as getter
+     * @reutrn (string) disable value
+     */
     disValue (prm) {
         try {
             let ret = this.member(
@@ -80,12 +119,20 @@ mf.effect.Margin = class extends mf.Effect {
                 ['Size'],
                 (undefined !== prm) ? mf.func.getSize(prm) : prm
             );
-            return (undefined !== ret) ? ret.toString() : undefined;
+            return (true === mf.func.isInclude(ret, 'Size')) ? ret.toString() : null;
         } catch (e) {
             console.error(e.stack);
         }
     }
     
+    /**
+     * margin type setter/getter
+     * when not specifying anything, everything becomes effective
+     * 
+     * @param p1 (string) margin type ('top', 'right', 'bottom', 'left')
+     * @param p1 (undefined) call as getter
+     * @return (string) margin type
+     */
     type (prm) {
         try {
             return this.member(
