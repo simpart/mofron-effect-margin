5046b00470c9520af0b083403b6e1ab5c010d0eb
taria <taria.ki4m@gmail.com>
2024-08-14 10:28:51 +0000

    supported v1.2

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
  * @brief margin effect
  * @author simpart
  */
-const mf = require('mofron');
 
-mf.effect.Margin = class extends mf.Effect {
+module.exports = class extends mofron.class.Effect {
     /**
      * initialize effect
      *
@@ -13,115 +12,44 @@ mf.effect.Margin = class extends mf.Effect {
      * @param p1 (string) margin type
      * @param p2 (string) margin value (css size)
      */
-    constructor (po, p2) {
+    constructor (p1, p2) {
         try {
             super();
-            this.name('Margin');
-            this.prmMap(['type', 'value']);
-            this.prmOpt(po, p2);
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    /**
-     * enable margin effect
-     *
-     * @note private method
-     */
-    enable (tgt) {
-        try {
-            let mg = (null === this.type()) ? 'margin' : 'margin' + '-' + this.type();
-            let setmgn = {};
-            setmgn[mg] = this.enaValue();
-            tgt.adom().style(setmgn);
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    /**
-     * disable margin effect
-     *
-     * @note private method
-     */
-    disable (tgt) {
-        try {
-            let mg = (null === this.type()) ? 'margin' : 'margin' + '-' + this.type();
-            let setmgn = {};
-            setmgn[mg] = this.disValue();
-            tgt.adom().style(setmgn);
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    /**
-     * margin value setter/getter
-     *
-     * @param p1 (string) enable margin value (size value of css)
-     * @param p1 (undefined) call as getter
-     * @param p2 (string) disable margin value (size value of css)
-     * @param p2 (undefined) call as getter
-     * @reutrn (array) [ enable value, disable value ]
-     */
-    value (ena, dis) {
-        try {
-            if ( (undefined === ena) &&
-                 (undefined === dis) ) {
-                /* getter */
-                return [this.enaValue(), this.disValue()];
+            this.modname("Margin");
+            this.shortForm("type", "value");
+        
+            this.transition([
+                "margin",
+                "margin-top",
+                "margin-bottom",
+                "margin-left",
+                "margin-right"
+            ]);
+
+	    this.confmng().add('type', { type:'string', select:['top', 'bottom', 'left', 'right'] });
+            this.confmng().add('value', { type:'size' });
+
+            if (0 < arguments.length) {
+                this.config(p1,p2);
             }
-            /* setter */
-            this.enaValue(ena);
-            this.disValue(dis);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
-    
-    /**
-     * enable margin value setter/getter
-     *
-     * @param p1 (string) enable margin value (size value of css)
-     * @param p1 (undefined) call as getter
-     * @reutrn (string) enable value
-     */
-    enaValue (prm) {
+
+    contents (cmp) {
         try {
-            let ret = this.member(
-                'enaValue',
-                ['Size'],
-                (undefined !== prm) ? mf.func.getSize(prm) : prm
-            );
-            return (true === mf.func.isInclude(ret, 'Size')) ? ret.toString() : null;
+	    let mgn_typ = 'margin';
+            if (null !== this.type()) {
+                mgn_typ += '-' + this.type()
+	    }
+	    let set_mgn = {};
+	    set_mgn[mgn_typ] = this.value();
+	    cmp.style(set_mgn);
         } catch (e) {
             console.error(e.stack);
             throw e;
-        }
-    }
-    
-    /**
-     * disable margin value setter/getter
-     *
-     * @param p1 (string) disable margin value (size value of css)
-     * @param p1 (undefined) call as getter
-     * @reutrn (string) disable value
-     */
-    disValue (prm) {
-        try {
-            let ret = this.member(
-                'disValue', 
-                ['Size'],
-                (undefined !== prm) ? mf.func.getSize(prm) : prm
-            );
-            return (true === mf.func.isInclude(ret, 'Size')) ? ret.toString() : null;
-        } catch (e) {
-            console.error(e.stack);
         }
     }
     
@@ -135,15 +63,18 @@ mf.effect.Margin = class extends mf.Effect {
      */
     type (prm) {
         try {
-            return this.member(
-                'type',
-                ['top', 'right', 'bottom', 'left'],
-                prm
-            );
+            return this.confmng('type', prm);
         } catch (e) {
             console.error(e.stack);
         }
     }
+
+    value (prm) {
+        try {
+            return this.confmng('value', prm);
+        } catch (e) {
+            console.error(e.stack);
+        }
+    } 
 }
-module.exports = mofron.effect.Margin;
 /* end of file */
